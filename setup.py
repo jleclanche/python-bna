64380b6d5dad486c300765e4ae3bd14b453ed3c7
Jerome Leclanche <adys@azura>
2010-10-01 02:31:29 +0100

    Add even more info in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@
 from distutils.core import setup
 
 setup(
-	name="python-bna",
-	packages=["bna"],
-	url="http://github.com/Adys/python-bna"
-	download_url="http://github.com/Adys/python-bna/tarball/master"
-	version="1.0",
+	name = "python-bna",
+	packages = ["bna"],
+	author = "Jerome Leclanche",
+	author_email = "adys.wh@gmail.com",
+	url = "http://github.com/Adys/python-bna",
+	download_url = "http://github.com/Adys/python-bna/tarball/master",
+	version = "1.0",
 )
