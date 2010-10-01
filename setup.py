367cf56fd1e5a965e7874ad66d0c6184cc3cf971
Jerome Leclanche <jerome@leclan.ch>
2010-10-01 02:26:29 +0100

    Add more info in setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,10 @@
 
 from distutils.core import setup
 
-setup(name="python-bna", packages=["bna"])
+setup(
+	name="python-bna",
+	packages=["bna"],
+	url="http://github.com/Adys/python-bna"
+	download_url="http://github.com/Adys/python-bna/tarball/master"
+	version="1.0",
+)
