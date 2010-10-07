e7575f72f8b6edf77fe69bf098abb5bb40c7d0cb
Jerome Leclanche <adys@azura>
2010-10-07 06:10:34 +0100

    Include README.rst as documentation in the setup script

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import os.path
 from distutils.core import setup
+
+README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 setup(
 	name = "python-bna",
@@ -9,5 +13,6 @@ setup(
 	author_email = "adys.wh@gmail.com",
 	url = "http://github.com/Adys/python-bna",
 	download_url = "http://github.com/Adys/python-bna/tarball/master",
+	long_description = README,
 	version = "1.0",
 )
