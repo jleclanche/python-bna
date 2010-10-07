e6442f20f20c646c41e0c03950a306d047c21e34
Jerome Leclanche <adys@azura>
2010-10-07 06:51:44 +0100

    Add classifiers and fix up the setup script

--- setup.py
+++ setup.py
@@ -6,14 +6,24 @@ from distutils.core import setup
 
 README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
+CLASSIFIERS = [
+	"Development Status :: 5 - Production/Stable",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: MIT License",
+	"Programming Language :: Python",
+	"Topic :: Security",
+	"Topic :: Security :: Cryptography",
+]
+
 setup(
 	name = "python-bna",
-	py_modules = ["bna"],
+	py_packages = ["bna"],
 	author = "Jerome Leclanche",
 	author_email = "adys.wh@gmail.com",
-	url = "http://github.com/Adys/python-bna",
-	download_url = "http://github.com/Adys/python-bna/tarball/master",
+	classifiers = CLASSIFIERS,
 	description = "Battle.net Authenticator routines in Python.",
+	download_url = "http://github.com/Adys/python-bna/tarball/master",
 	long_description = README,
-	version = "1.1",
+	url = "http://github.com/Adys/python-bna",
+	version = "1.1.2",
 )
