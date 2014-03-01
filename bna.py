bc715400ea5603a6905c584a24eb7a0758905985
Jerome Leclanche <adys.wh@gmail.com>
2014-03-01 17:09:53 +0000

    Update Github URL and email

--- bna.py
+++ bna.py
@@ -8,7 +8,7 @@ Note: Link likely dead. Check webarchive.
 """
 
 __author__ = "Jerome Leclanche"
-__email__ = "jerome.leclanche+pypi@gmail.com"
+__email__ = "jerome@leclan.ch"
 __version__ = "3.2"
 
 import hmac
