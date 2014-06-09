aa509757ca44bab87f866bf8f27bc536ccc4189c
Jerome Leclanche <jerome@leclan.ch>
2014-06-09 20:45:57 +0100

    Added Python 3.4 classifier

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ CLASSIFIERS = [
 	"Programming Language :: Python :: 2.7",
 	"Programming Language :: Python :: 3",
 	"Programming Language :: Python :: 3.3",
+	"Programming Language :: Python :: 3.4",
 	"Topic :: Security",
 	"Topic :: Security :: Cryptography",
 ]
